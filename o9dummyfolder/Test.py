8b25589e5aaf18d841cfa8ba12e64953bfafefa5
SandeepAroha07 <54171260+SandeepAroha07@users.noreply.github.com>
2024-10-28 11:36:02 +0530

    Update Test.py

--- o9dummyfolder/Test.py
+++ o9dummyfolder/Test.py
@@ -4,7 +4,7 @@ class Test:
         self.ds = []
 
     def get(self, string_name):
-        fmt = "test name instance [{}] is getting you {}".format(self.name, string_name)
+        fmt = "test name instance [{}] is getting you {} , In Code Lib".format(self.name, string_name)
         return fmt
 
     def add(self, string_name):
@@ -15,7 +15,7 @@ class Test:
         fmt = ""
         for s in self.ds:
             fmt = fmt + s
-        fmt = "test name instance [{}] is getting you {}".format(self.name, fmt)
+        fmt = "test name instance [{}] is getting you {} , In Code Lib".format(self.name, fmt)
         return fmt
     def show_msg():
         msg = "########################################## This is a test message ######################################"
